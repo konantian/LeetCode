e0598cf39a3ed9a0b448954b0212fa16e8e7ac5c
王源 <konantian@wangyuandeMacBook-Air.local>
2018-05-26 18:03:26 +0800

    update codes

---
+++ Array/TwoSum.py
@@ -0,0 +1,12 @@
+def twoSum(nums, target):
+	"""
+	:type nums: List[int]
+	:type target: int
+	:rtype: List[int]
+	"""
+	for i in range(len(nums)):
+		for j in range(i+1,len(nums)):
+			if nums[i]+nums[j]==target:
+				return [i,j]
+
+print(twoSum([3,2,4],6))
