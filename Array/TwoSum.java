4bb1120ace40380debb281b42e6764ce9284c0cb
王源 <konantian@wangyuandeMacBook-Air.local>
2018-05-27 12:39:51 +0800

    update codes

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -7,33 +7,38 @@ Algorithm:
 The brute force approach is simple. Loop through each element x
 and find if there is another value that their sum is target
 
-Complexity Analysis
+Complexity Analysis:
 
-Time complexity : O(n^2)
-​For each element, we try to find its complement by looping through the 
-rest of array which takes O(n)O(n) time. Therefore, the time complexity is O(n^2)
+Time complexity : O(n). We traverse the list containing nn elements 
+exactly twice. Since the hash table reduces the look up time to O(1), 
+the time complexity is O(n).
 
-Space complexity : O(1)
+Space complexity : O(n). The extra space required depends on the 
+number of items stored in the hash table, which stores exactly nn elements.
 */
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Arrays;
 public class TwoSum {
     public static void main(String[] args){
         int array[] = {2,7,11,15};
         int target = 9;
-        int index[]=twosum(array,target);
+        int index[]=twoSum(array,target);
         System.out.println(Arrays.toString(index));
         
     }
 
-    public static int[] twosum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
-        for (int j = i + 1; j < nums.length; j++) {
-            if (nums[j] == target - nums[i]) {
-                return new int[] { i, j };
-            }
+        map.put(nums[i], i);
+    }
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement) && map.get(complement) != i) {
+            return new int[] { i, map.get(complement) };
         }
     }
     throw new IllegalArgumentException("No two sum solution");
-}
+    }
 }
