82bbf02abd363dbe3bef086fc8c097836b53dd2d
王源 <konantian@wangyuandeMacBook-Air.local>
2018-06-05 14:26:21 +0800

    update java solution

--- Array/rotateArray.java
+++ Array/rotateArray.java
@@ -4,7 +4,7 @@ public class rotateArray{
 	public static void main(String[] args){
 		int array[]={1,2,3,4,5,6,7};
 		int k = 3;
-		rotate(array,k);
+		rotateB(array,k);
 		System.out.println(Arrays.toString(array));
 	}
 
@@ -16,6 +16,24 @@ public class rotateArray{
 
 		for(int i = 0;i<nums.length;i++){
 			nums[i] = a[i];
+		}
+	}
+
+	public static void rotateB(int nums[],int k){
+		k %= nums.length;
+		reverse(nums,0,nums.length-1);
+		reverse(nums,0,k-1);
+		reverse(nums,k,nums.length-1);
+	}
+
+	public static void reverse(int nums[],int start,int end){
+		while(start < end){
+			int temp = nums[start];
+			nums[start] = nums[end];
+			nums[end]=temp;
+			start++;
+			end--;
+
 		}
 	}
 }
