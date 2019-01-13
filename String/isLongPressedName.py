49068d2e14a844d2d9c67742fb39ad2aedeb57d0
Yuan Wang <wang17@ualberta.ca>
2019-01-12 23:58:52 -0700

    update problem 924

--- String/isLongPressedName.py
+++ String/isLongPressedName.py
@@ -24,10 +24,10 @@ Date   : 2019-01-12
 '''
 
 def isLongPressedName(name, typed):
-        g1 = [(k, len(list(grp))) for k, grp in itertools.groupby(name)]
-        g2 = [(k, len(list(grp))) for k, grp in itertools.groupby(typed)]
-        if len(g1) != len(g2):
-            return False
+    g1 = [(k, len(list(grp))) for k, grp in itertools.groupby(name)]
+    g2 = [(k, len(list(grp))) for k, grp in itertools.groupby(typed)]
+    if len(g1) != len(g2):
+        return False
 
-        return all(k1 == k2 and v1 <= v2
-                   for (k1,v1), (k2,v2) in zip(g1, g2))
+    return all(k1 == k2 and v1 <= v2
+        	for (k1,v1), (k2,v2) in zip(g1, g2))
