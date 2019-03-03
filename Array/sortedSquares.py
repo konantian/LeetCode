6de32677f64028301ed4a247daaab36ed82e772e
Yuan Wang <wang17@ualberta.ca>
2019-03-03 00:12:29 -0700

    add another solution for problem 977

--- Array/sortedSquares.py
+++ Array/sortedSquares.py
@@ -27,3 +27,18 @@ def sortedSquares(A):
 
     return sorted([i*i for i in A])
 
+#Solution with two pointer
+def sortedSquares(self, A):
+	answer = [0] * len(A)
+	l, r = 0, len(A) - 1
+	while l <= r:
+		left, right = abs(A[l]), abs(A[r])
+		if left > right:
+			answer[r - l] = left * left
+			l += 1
+		else:
+			answer[r - l] = right * right
+			r -= 1
+	return answer
+
+
