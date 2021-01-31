6a7d3bc9ba7b08c61576274deaea109896559e3c
Yuan Wang <35284474+konantian@users.noreply.github.com>
2021-01-31 23:40:15 +0800

    Update reverseList.py

--- LinkedList/reverseList.py
+++ LinkedList/reverseList.py
@@ -14,25 +14,17 @@
 *A linked list can be reversed either iteratively or recursively. Could you implement both?
 **********************************************************************************/
 '''
-from SLinkedList import SLinked_List
+from SLinkedList import SLinked_List, ListNode
 
-def reverseList(head):
-	"""
-	:type head: ListNode
-	:rtype: ListNode
-	"""
-	currentNode=head
-	if head != None:
-		nextNode = head.next
-		currentNode.next = None
-
-		while nextNode != None:
-			aux = nextNode.next
-			nextNode.next = currentNode
-			currentNode = nextNode
-			nextNode = aux
-	
-	return currentNode
+def reverseList(head: ListNode) -> ListNode:
+	current = head
+	previous = None
+	while current:
+		temp = current.next
+		current.next = previous
+		previous = current
+		current = temp
+	return previous
 
 elements=[1,2,3,4,5]
 
